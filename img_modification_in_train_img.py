5b474fcb2790b459ca4f0330156128b2cc917bb7
siddhuravi5 <58221167+siddhuravi5@users.noreply.github.com>
2020-01-09 18:32:40 +0530

    Face-Recognition-Assignment completed

---
+++ img_modification_in_train_img.py
@@ -0,0 +1,58 @@
+import cv2
+import numpy as np
+frame=cv2.imread('C:\\Users\\Siddharth\\Pictures\\2018-02\\Jamie_Before.jpg')
+#frame=cv2.resize(frame,(500,800))
+face_cascade=cv2.CascadeClassifier('C:\\Users\\Siddharth\\Documents\\haarcascade_frontalface_default.xml')
+eyes_cascade=cv2.CascadeClassifier('C:\\Users\\Siddharth\\Documents\\haarcascade_eye.xml')
+#eyes_cascade=cv2.CascadeClassifier(cv2.data.haarcascades +'haarcascade_eye.xml')
+f3=cv2.CascadeClassifier('C:\\Users\\Siddharth\\Documents\\haarcascade_mcs_nose.xml')
+imgg=cv2.imread('C:\\Users\\Siddharth\\Pictures\\2018-02\\mustache2.jpg')
+img2=cv2.imread('C:\\Users\\Siddharth\\Pictures\\2018-02\\glasses.jpg')
+while True:
+    gray_frame=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    faces=face_cascade.detectMultiScale(gray_frame,1.3,5)
+    if(len(faces)==0):
+       continue
+    faces=sorted(faces,key=lambda x:x[2]*x[3])
+    #print(faces)
+
+    for face in faces[-1:]:
+        x,y,w,h=face
+        a=face
+        #cv2.rectangle(gray_frame,(x,y),(x+w,y+h),(10,15,205),-1)
+    roi_gray = gray_frame[y:y+h, x:x+w]
+    roi_color = frame[y:y+h, x:x+w]
+    
+    eyes = eyes_cascade.detectMultiScale(gray_frame)
+    print(eyes)
+    for eye in eyes:
+            ex,ey,ew,eh=eye
+            if(ex>20 and ey<600 and ex<350):
+                #cv2.rectangle(frame,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+                X1=ex
+                Y1=ey
+                X2=X1+2*ew+25
+                Y2=ey+eh
+                img2=cv2.resize(img2,(X2-X1,Y2-Y1),interpolation=True)
+                frame[Y1:Y2,X1:X2]=cv2.bitwise_and(frame[Y1:Y2,X1:X2],img2,mask=None)
+
+    nose = f3.detectMultiScale(gray_frame)
+    #print(eyes)
+    for (ex,ey,ew,eh) in nose[:1]:
+        
+        #cv2.rectangle(gray_frame,(ex-15,ey+55),(ex+ew+15,ey+eh),(0,255,0),2)
+        x1=ex-15
+        y1=ey+53
+        x2=ex+ew+15
+        y2=ey+eh+20
+        imgg=cv2.resize(imgg,(x2-x1,y2-y1),interpolation=cv2.INTER_AREA)
+        #frame[y1:y2,x1:x2]=cv2.addWeighted(frame[y1:y2,x1:x2],-5,imgg,-10,0,frame)
+        frame[y1:y2,x1:x2]= cv2.bitwise_and(frame[y1:y2,x1:x2],imgg , mask=None)
+    cv2.imshow("gray_frame",frame)
+    key_pressed=cv2.waitKey(1)
+    if(key_pressed==ord('q')):
+        break
+    break
+    
+
+print("hello")
